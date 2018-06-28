de8aa95a9778fea8f9b904f5275919ce96eb8d02
alisha122 <alishagupta262@gmail.com>
2018-06-28 12:14:11 +0530

    new commit

--- POS.go
+++ POS.go
@@ -19,37 +19,22 @@ type POSData struct {
 	APIEnabled           string  `json:"APIEnabled"`
 	APIVersionsSupported string  `json:"APIVersionsSupported"`
 	Network              string  `json:"Network"`
-	Url                  string  `json:"URL"`
+	URL                  string  `json:"URL"`
 	Launched             float64 `json:"Launched"`
 	LastUpdated          float64 `json:"LastUpdated"`
 	Immature             float64 `json:"Immature"`
-	Live                 string  `json: "Live"`
+	Live                 string  `json:"Live"`
 	Voted                string  `json:"Voted"`
 	Missed               string  `json:"Missed"`
 	PoolFees             string  `json:"PoolFees"`
 	ProportionLive       float64 `json:"ProportionLive"`
-	ProportionMissed     float64 `json: "ProportionMissed"`
+	ProportionMissed     float64 `json:"ProportionMissed"`
 	UserCount            int     `json:"UserCount"`
 	UserCountActive      int     `json:"UserCountActive"`
 }
 
-type POSID struct {
-	Ethan    map[string]*POSData `json:"Ethan"`
-	Papa     map[string]*POSData `json:"Papa"`
-	Echo     map[string]*POSData `json:"Echo"`
-	Mike     map[string]*POSData `json:"Mike"`
-	Oscar    map[string]*POSData `json:"Oscar"`
-	Golf     map[string]*POSData `json:"Golf"`
-	November map[string]*POSData `json:"November"`
-	Ray      map[string]*POSData `json:"Ray"`
-	Hotel    map[string]*POSData `json:"Hotel"`
-	Juliett  map[string]*POSData `json:"Juliett"`
-	Lima     map[string]*POSData `json:"Lima"`
-	India    map[string]*POSData `json:"India"`
-	Bravo    map[string]*POSData `json:"Bravo"`
-	Delta    map[string]*POSData `json:"Delta"`
-	Kilo     map[string]*POSData `json:"Kilo"`
-}
+//POSID Each POS Id maps to the POS data
+type POSID map[string]POSData
 
 func (p *POS) getPOS() {
 	url := url
@@ -66,7 +51,8 @@ func (p *POS) getPOS() {
 	var data POSID
 	json.Unmarshal(body, &data)
 
-	fmt.Printf("Results: %v\n", data)
+	fmt.Println(string(body))
+	fmt.Printf("Results: %v\n", data["Papa"].URL)
 
 	//Loop over the entire list to insert data into the table
 	for i := 0; i < 15; i++ {
